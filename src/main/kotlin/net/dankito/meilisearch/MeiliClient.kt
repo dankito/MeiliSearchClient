83d386ef4e18bc76f5ae6af824858464d013b050
dankito <git@dankito.net>
2026-06-06 05:56:52 +0200

    Implemented convenience method to configure index

--- src/main/kotlin/net/dankito/meilisearch/MeiliClient.kt
+++ src/main/kotlin/net/dankito/meilisearch/MeiliClient.kt
@@ -19,6 +19,44 @@ open class MeiliClient(
     internal val client = Client(Config(meiliHost, meiliApiKey))
 
 
+    suspend fun configureIndex(indexName: String, searchableAttributes: Collection<String> = emptyList(), // Welche Felder gehen in die Volltext-Suche (Ranking nach Reihenfolge)
+                               filterableAttributes: Collection<String> = emptyList(), // Welche Felder dürfen gefiltert/gefacettiert werden
+                               sortableAttributes: Collection<String> = emptyList(), // Welche Felder können sortiert werden
+    ) {
+        val index = client.index(indexName)
+
+        // Welche Felder dürfen gefiltert/gefacettiert werden
+        if (filterableAttributes.isNotEmpty()) {
+            val taskInfo = index.updateFilterableAttributesSettings(filterableAttributes.toTypedArray())
+            val task = waitForTaskSuccessOrThrow(taskInfo)
+        }
+
+        // Welche Felder können sortiert werden
+        if (sortableAttributes.isNotEmpty()) {
+            val taskInfo = index.updateSortableAttributesSettings(sortableAttributes.toTypedArray())
+            waitForTaskSuccessOrThrow(taskInfo)
+        }
+
+        // Welche Felder gehen in die Volltext-Suche (Ranking nach Reihenfolge)
+        if (searchableAttributes.isNotEmpty()) {
+            val taskInfo = index.updateSearchableAttributesSettings(searchableAttributes.toTypedArray())
+            waitForTaskSuccessOrThrow(taskInfo)
+        }
+
+
+//        val embedderSettings = mapOf(
+//            "default" to mapOf(
+//                "source" to "openAi",
+//                "apiKey" to openAiApiKey,
+//                "model" to "text-embedding-3-small",
+//                "documentTemplate" to "{{doc.title}} {{doc.overviewMarkdown}} {{doc.tripDescriptionMarkdown}}",
+//            )
+//        )
+//        // Per REST da das Java-SDK Embedder noch nicht vollständig unterstützt:
+//        index.rawSearch("""{"embedders": ${objectMapper.writeValueAsString(embedderSettings)}}""")
+    }
+
+
     suspend fun waitForTask(taskInfo: TaskInfo, timeoutMs: Long = 60_000, intervalMs: Long = 200) =
         waitForTask(taskInfo.taskUid, timeoutMs, intervalMs)
 
