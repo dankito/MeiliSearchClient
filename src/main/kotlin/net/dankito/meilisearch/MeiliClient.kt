b62413927e1b0d02731aade2965d72707a0e426e
dankito <git@dankito.net>
2026-06-24 02:21:59 +0200

    Extracted constants for task timeout and check interval

--- src/main/kotlin/net/dankito/meilisearch/MeiliClient.kt
+++ src/main/kotlin/net/dankito/meilisearch/MeiliClient.kt
@@ -29,6 +29,12 @@ open class MeiliClient(
     }
 ) {
 
+    companion object {
+        const val DefaultTaskTimeoutMs = 60_000
+        const val DefaultTaskCheckIntervalMs = 25
+    }
+
+
     val client = Client(Config(meiliHost, meiliApiKey))
 
 
@@ -162,10 +168,10 @@ open class MeiliClient(
     }
 
 
-    suspend fun waitForTask(taskInfo: TaskInfo, timeoutMs: Long = 60_000, intervalMs: Long = 25) =
+    suspend fun waitForTask(taskInfo: TaskInfo, timeoutMs: Int = DefaultTaskTimeoutMs, intervalMs: Int = DefaultTaskCheckIntervalMs) =
         waitForTask(taskInfo.taskUid, timeoutMs, intervalMs)
 
-    suspend fun waitForTask(taskUid: Int, timeoutMs: Long = 60_000, intervalMs: Long = 25): TaskResult {
+    suspend fun waitForTask(taskUid: Int, timeoutMs: Int = DefaultTaskTimeoutMs, intervalMs: Int = DefaultTaskCheckIntervalMs): TaskResult {
         val deadline = System.currentTimeMillis() + timeoutMs
         val interval = intervalMs.milliseconds
 
@@ -182,10 +188,10 @@ open class MeiliClient(
         return TaskFailure(taskUid, null, "Meilisearch task $taskUid timed out after ${timeoutMs}ms")
     }
 
-    suspend fun waitForTaskSuccessOrThrow(task: TaskInfo, timeoutMs: Long = 60_000, intervalMs: Long = 25) =
+    suspend fun waitForTaskSuccessOrThrow(task: TaskInfo, timeoutMs: Int = DefaultTaskTimeoutMs, intervalMs: Int = DefaultTaskCheckIntervalMs) =
         waitForTaskSuccessOrThrow(task.taskUid, timeoutMs, intervalMs)
 
-    suspend fun waitForTaskSuccessOrThrow(taskUid: Int, timeoutMs: Long = 60_000, intervalMs: Long = 25): Task {
+    suspend fun waitForTaskSuccessOrThrow(taskUid: Int, timeoutMs: Int = DefaultTaskTimeoutMs, intervalMs: Int = DefaultTaskCheckIntervalMs): Task {
         val taskResult = waitForTask(taskUid, timeoutMs, intervalMs)
 
         when (taskResult) {
